bad1032418d886fbf4525b45c4a446d9075d3c93
tomfegan <tomfegan83@gmail.com>
2024-06-28 21:19:11 +0100

    feat: added navigateToPreviousActivity() and navigateToSpotChoiceActivity() methods

---
+++ app/src/main/java/com/example/surfeillancefrontend/model/Location.java
@@ -0,0 +1,159 @@
+package com.example.surfeillancefrontend.model;
+
+public class Location {
+
+
+    String name;
+    String details;
+    String requestTime;
+    String latitude;
+    String longitude;
+    String timezone;
+    Double waveHeight;
+    String waveDirection;
+    Double wavePeriod;
+    Double windSpeed;
+    Double gusts;
+    Double highTideheight;
+    String highTideTime;
+    Double lowTideHeight;
+    String lowTideTime;
+
+    public Location(String name, String details, String requestTime, String latitude, String longitude, String timezone, Double waveHeight, String waveDirection, Double wavePeriod, Double windSpeed, Double gusts, Double highTideheight, String highTideTime, Double lowTideHeight, String lowTideTime) {
+        this.name = name;
+        this.details = details;
+        this.requestTime = requestTime;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timezone = timezone;
+        this.waveHeight = waveHeight;
+        this.waveDirection = waveDirection;
+        this.wavePeriod = wavePeriod;
+        this.windSpeed = windSpeed;
+        this.gusts = gusts;
+        this.highTideheight = highTideheight;
+        this.highTideTime = highTideTime;
+        this.lowTideHeight = lowTideHeight;
+        this.lowTideTime = lowTideTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(String requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public Double getWaveHeight() {
+        return waveHeight;
+    }
+
+    public void setWaveHeight(Double waveHeight) {
+        this.waveHeight = waveHeight;
+    }
+
+    public String getWaveDirection() {
+        return waveDirection;
+    }
+
+    public void setWaveDirection(String waveDirection) {
+        this.waveDirection = waveDirection;
+    }
+
+    public Double getWavePeriod() {
+        return wavePeriod;
+    }
+
+    public void setWavePeriod(Double wavePeriod) {
+        this.wavePeriod = wavePeriod;
+    }
+
+    public Double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public Double getGusts() {
+        return gusts;
+    }
+
+    public void setGusts(Double gusts) {
+        this.gusts = gusts;
+    }
+
+    public Double getHighTideheight() {
+        return highTideheight;
+    }
+
+    public void setHighTideheight(Double highTideheight) {
+        this.highTideheight = highTideheight;
+    }
+
+    public String getHighTideTime() {
+        return highTideTime;
+    }
+
+    public void setHighTideTime(String highTideTime) {
+        this.highTideTime = highTideTime;
+    }
+
+    public Double getLowTideHeight() {
+        return lowTideHeight;
+    }
+
+    public void setLowTideHeight(Double lowTideHeight) {
+        this.lowTideHeight = lowTideHeight;
+    }
+
+    public String getLowTideTime() {
+        return lowTideTime;
+    }
+
+    public void setLowTideTime(String lowTideTime) {
+        this.lowTideTime = lowTideTime;
+    }
+}
--- app/src/main/java/com/example/surfeillancefrontend/ui/SpotChoiceActivity.java
+++ app/src/main/java/com/example/surfeillancefrontend/ui/SpotChoiceActivity.java
@@ -1,7 +1,11 @@
 package com.example.surfeillancefrontend.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.surfeillancefrontend.MainActivity;
 import com.example.surfeillancefrontend.R;
 
 public class SpotChoiceActivity extends AppCompatActivity {
@@ -10,6 +14,22 @@ public class SpotChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spot_choice);
+        returnToHomePage();
 
     }
+
+    public void returnToHomePage() {
+        ImageButton back = (ImageButton) findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity i.e. return to previous screen
+                Intent intent = new Intent(SpotChoiceActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
 }
