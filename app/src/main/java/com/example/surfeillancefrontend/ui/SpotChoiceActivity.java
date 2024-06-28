46c6e81f0f2b732c8ed5e44ce469a7e024125efd
tomfegan <tomfegan83@gmail.com>
2024-06-28 15:48:46 +0100

    up to date main branch

---
+++ app/src/main/java/com/example/surfeillancefrontend/ui/SpotChoiceActivity.java
@@ -0,0 +1,15 @@
+package com.example.surfeillancefrontend.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.surfeillancefrontend.R;
+
+public class SpotChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spot_choice);
+
+    }
+}
