d6c64ef5e16e22c7c7d159d015f59b0ef2cd7962
ludic <notetakingforeggs@gmail.com>
2024-07-03 10:33:51 +0100

    Feat: changed to getting trips by user id as Query Param

--- app/src/main/java/com/example/surfeillancefrontend/service/TripApiService.java
+++ app/src/main/java/com/example/surfeillancefrontend/service/TripApiService.java
@@ -3,12 +3,17 @@ package com.example.surfeillancefrontend.service;
 import com.example.surfeillancefrontend.model.data.Trip;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 import java.util.List;
 
 public interface TripApiService {
+//    @GET("faketrips")
+//    Call<List<Trip>> getTrips();
+
+
     @GET("faketrips")
-    Call<List<Trip>> getTrips();
+    Call<List<Trip>> getTripsByUserId(@Query("userid") String userid);
 
 
 }
