c263cb13919516f42e79730422d0d63b9b531634
franc <francisadewale1992@gmail.com>
2024-07-08 10:47:43 +0100

    update: front end OAUTH

--- app/src/main/java/com/example/surfeillancefrontend/service/ApiClient.java
+++ app/src/main/java/com/example/surfeillancefrontend/service/ApiClient.java
@@ -1,16 +1,42 @@
 
 package com.example.surfeillancefrontend.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class ApiClient {
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
+
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+//            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+//                @Override
+//                public Response intercept(Chain chain) throws IOException {
+//                    Request newRequest  = chain.request().newBuilder()
+//                            .addHeader("Authorization", "Bearer " + token)
+//                            .build();
+//                    return chain.proceed(newRequest);
+//                }
+//            }).build();
+
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .addInterceptor(loggingInterceptor)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:8080/")
+                    .baseUrl("http://172.23.223.86:8080/oauth/")
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
---
+++ app/src/main/java/com/example/surfeillancefrontend/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.surfeillancefrontend.service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface AuthService {
+    @GET("hello")
+    Call<String> authenticate(@Header("Authorization") String authHeader);
+}
